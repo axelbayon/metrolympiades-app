e8a65c0e95f2e332ab73e37d7580a2e5c1441b5e
Eliot Petrus <petrus221@campus.unicaen.fr>
2024-03-21 16:14:09 +0100

    feat: visualizing/modifing team's members

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Teams from '../views/TeamsView.vue';
+import teamSettings from '../views/TeamSettingsView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'Teams',
-      component: Teams,
+      name: 'TeamSettings',
+      component: teamSettings,
     }
   ]
 })
---
+++ src/views/TeamSettingsView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {onMounted} from 'vue';
+import {fetchMembers/*, updateMembers*/, members} from '@/api/team';
+
+onMounted(async () => {
+    await fetchMembers()
+})
+
+// function addMembers(members){
+//     members.push("zzz")
+//     InsertMembers(members)
+// }
+
+// function update(members){
+//     updateMembers(members)
+// }
+
+</script>
+
+
+<template>
+    <div>
+        <label for="members">Members :</label>
+        <div v-for="(member, i) in members" :key="i">
+            <input type="text" v-model="members[i]" @change="update(members)"/>
+            
+        </div>
+        {{members}}
+        <!--<button @click="addMember(members)">add a members +</button>-->
+    </div>
+</template>
