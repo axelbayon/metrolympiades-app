5f4833e2c45a13d77883a9be89fb9842c932195f
petrus221 <eliot.petrus@etu.unicaen.fr>
2024-03-27 12:04:23 +0100

    feat: TeamSettings modify team name

--- src/api/team.js
+++ src/api/team.js
@@ -3,6 +3,18 @@ import { ref } from 'vue';
 
 export const members = ref([])
 
+export const teamName = ref ("")
+
+export const fetchTeamName = async () => {
+    const {error, data} = await supabase.from('teams').select('name').eq('leader', 'af15a5b4-3d82-4967-bb2c-e01fffc69aab').single()
+
+    if (error){
+        console.error('Error fetching team name', error)
+    } else { 
+        teamName.value = data.name ?? ""
+    }
+}
+
 export const fetchMembers = async () => {
     const {error, data} = await supabase.from('teams').select('members').eq('leader', 'af15a5b4-3d82-4967-bb2c-e01fffc69aab').single()
 
@@ -17,5 +29,12 @@ export const updateMembers = async (newMembers) => {
     const {error} = await supabase.from('teams').update({members: newMembers}).eq('leader', 'af15a5b4-3d82-4967-bb2c-e01fffc69aab')
     if (error){
         console.error('Error updating members', error)
+    }
+}
+
+export const updateTeamName = async (newName) => {
+    const {error} = await supabase.from('teams').update({name: newName}).eq('leader', 'af15a5b4-3d82-4967-bb2c-e01fffc69aab')
+    if (error){
+        console.error('Error updating team name', error)
     }
 }
--- src/views/TeamSettingsView.vue
+++ src/views/TeamSettingsView.vue
@@ -1,8 +1,9 @@
 <script setup>
 import {onMounted} from 'vue';
-import {fetchMembers, updateMembers, members} from '@/api/team';
+import {fetchTeamName, fetchMembers, updateTeamName, updateMembers, members, teamName} from '@/api/team';
 
 onMounted(async () => {
+    await fetchTeamName()
     await fetchMembers()
 
     if (!members.value.length) {
@@ -16,8 +17,12 @@ function addMembers(members){
     updateMembers(members)
 }
 
-function update(members){
+function ModifyMembers(members){
     updateMembers(members)
+}
+
+function ModifyTeamName(teamName){
+    updateTeamName(teamName)
 }
 
 function deleteMembers(members, index){
@@ -29,9 +34,15 @@ function deleteMembers(members, index){
 
 <template>
     <div>
+        <label for="members">{{ teamName }}</label>
+    </div>
+    <div>
+        <input type="text" v-model="teamName" @input="ModifyTeamName(teamName)"/>
+    </div>
+    <div>
         <label for="members">Members :</label>
         <div v-for="(member, i) in members" :key="i">
-            <input type="text" v-model="members[i]" @input="update(members)"/>
+            <input type="text" v-model="members[i]" @input="ModifyMembers(members)"/>
             <button v-if="i != 0" @click="deleteMembers(members, i)">[X]</button>
         </div>
         <button v-if="members.length < 5" @click="addMembers(members)">add a members +</button>
