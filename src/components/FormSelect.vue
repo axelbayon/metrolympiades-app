a2b89339572e72f2acae10c90da53d43b04b2c78
Faboo <fabienleroy2004@gmail.com>
2024-03-27 15:01:35 +0100

    joli form

--- src/components/FormSelect.vue
+++ src/components/FormSelect.vue
@@ -13,8 +13,8 @@
 
 <template>
     <div class="select-component">
-      <label class="block text-white">{{ label }}</label>
-      <select :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" class="flex w-4/6 p-2 mx-auto text-center text-white rounded-md bg-slate-600">
+      <select :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" v-bind="$attrs" class="flex w-4/6 p-2 m-2 mx-auto text-center text-white rounded-md bg-slate-600">
+        <option value="" disabled selected>{{ label }}</option>
         <option v-for="option in options" :value="option" :key="option">{{ option }}</option>
       </select>
     </div>
