f6bec585deb05856f1e68717aa0e011a1c20d0bb
Faboo <fabienleroy2004@gmail.com>
2024-03-28 11:26:38 +0100

    feat: bug user get my team

--- src/api/apiRankings.js
+++ src/api/apiRankings.js
@@ -27,3 +27,19 @@ export const  getResult= async() => {
     }
     return data
 }
+
+export const getMyTeam = async(user) => {
+    try {
+        const teams = await getTeam(); // Assurez-vous que getTeam() est une méthode définie dans vos données ou dans vos méthodes
+  
+        const userTeam = teams.find(team => team.id === user.teamId); // Supposons que chaque utilisateur a un attribut teamId correspondant à l'id de l'équipe
+  
+        if (userTeam) {
+          return userTeam;
+        } else {
+          throw new Error("L'équipe de l'utilisateur n'a pas été trouvée.");
+        }
+      } catch (error) {
+        console.error("Erreur lors de la récupération de l'équipe pour l'utilisateur:", error);
+      }
+}
