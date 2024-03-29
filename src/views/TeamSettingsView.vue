cd6022cd1dc2739d031484774ce1ed4c5cfcb9ba
Bayon Axel <axel.bayon14@gmail.com>
2024-03-29 10:01:31 +0100

    feat: adding light button

--- src/views/TeamSettingsView.vue
+++ src/views/TeamSettingsView.vue
@@ -31,19 +31,20 @@ function deleteMembers(members, index){
 }
 </script>
 
-
 <template>
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <div class="w-full max-w-md p-5 mx-auto my-5 transition-all duration-500 ease-in-out transform bg-white rounded-md shadow-md hover:scale-105">
             <div class="mb-4">
-                <label class="block mb-2 text-sm font-bold text-gray-700" for="members">{{ teamName }}</label>
+                <label class="block mb-2 text-sm font-bold text-gray-700" for="teamName">{{ teamName }}</label>
                 <input class="w-full px-3 py-2 leading-tight text-gray-700 transition-all duration-300 ease-in-out border rounded shadow-md appearance-none focus:outline-none focus:shadow-outline" type="text" v-model="teamName" @input="ModifyTeamName(teamName)"/>
             </div>
             <div>
                 <label class="block mb-2 text-sm font-bold text-gray-700" for="members">Members :</label>
-                <div v-for="(member, i) in members" :key="i" class="mb-4">
+                <div v-for="(member, i) in members" :key="i" class="relative mb-4">
                     <input class="w-full px-3 py-2 leading-tight text-gray-700 transition-all duration-300 ease-in-out border rounded shadow-md appearance-none focus:outline-none focus:shadow-outline" type="text" v-model="members[i]" @input="ModifyMembers(members)"/>
-                    <button class="px-4 py-2 mt-2 font-bold text-white transition-all duration-300 ease-in-out transform bg-red-500 rounded hover:bg-red-700 focus:outline-none focus:shadow-outline hover:scale-105" v-if="i != 0" @click="deleteMembers(members, i)">Delete</button>
+                    <button class="absolute top-0 right-0 p-1 mt-2 mr-2 text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline" @click="deleteMembers(members, i)">
+                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6.707 6.293a1 1 0 011.414 0L10 8.586l2.879-2.88a1 1 0 111.414 1.414L11.414 10l2.88 2.879a1 1 0 11-1.414 1.414L10 11.414l-2.879 2.88a1 1 0 01-1.414-1.414L8.586 10 5.707 7.121a1 1 0 010-1.414z"/></svg>
+                    </button>
                 </div>
                 <button class="px-4 py-2 font-bold text-white transition-all duration-300 ease-in-out transform bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline hover:scale-105" v-if="members.length < 5" @click="addMembers(members)">Add a member</button>
             </div>
